12cd4c03d3d4facaf494d02adf3f2fa675760cfc
Oilman <book321321@outlook.com>
2021-07-31 04:20:14 -0500

    Fix bug

--- Scripts/PipeManager.gd
+++ Scripts/PipeManager.gd
@@ -3,7 +3,8 @@ extends Node2D
 # Declare member variables here.
 const DEBUG = true
 
-var Pipes = preload("res://Scenes/PipeNode2D.tscn)
+var Pipes = preload("res://Scenes/PipeNode2D.tscn")
+
 onready var pipe_in_timer = get_node("PipeInTimer")
 
 # Called when the node enters the scene tree for the first time.

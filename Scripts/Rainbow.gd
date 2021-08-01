c3cf81117ceac003284405cd62afd0d652dd5b04
Oilman <book321321@outlook.com>
2021-07-31 20:56:38 -0500

    Bug Fix

---
+++ Scripts/Rainbow.gd
@@ -0,0 +1,11 @@
+extends Sprite
+
+export var RAINBOW_SPEED = 20
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
